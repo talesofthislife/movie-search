8b7967f4edf818a6f68c9e44066ed8f270c3c6d0
Maria Nikolaidou <marnicolaidou@gmail.com>
2024-12-27 18:15:52 +0100

    Clean up style and move font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 // https://tailwindcss.com/docs/guides/vite#vue
 
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,8 +9,22 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      orange: colors.orange,
+    },
   },
   plugins: [],
 }
-
