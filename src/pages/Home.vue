3325a5166b9a1f01f92e19cc040a1fd77964ba45
Maria Nikolaidou <marnicolaidou@gmail.com>
2025-01-03 15:04:47 +0100

    Fix homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,7 +8,7 @@ import ListPopularMovies from '../components/movies/ListPopularMovies.vue';
     <!-- px-4: Adds horizontal padding inside the container for better spacing on smaller screens. -->
     <!-- py-8: Adds vertical padding for spacing above and below the content. -->
     <div class="container mx-auto max-w-5xl px-4 py-8">
-        <h1 class="text-2xl font-bold mb-6 text-center mb-8">Find your next Movie or Series</h1>
+        <h1 class="text-2xl font-bold text-center mb-8">Find your next Movie or Series</h1>
         <ListPopularMovies></ListPopularMovies>
     </div>
 </template>
