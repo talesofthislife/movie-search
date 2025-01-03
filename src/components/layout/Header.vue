051a66edd95bea1070bd84cf7ca718c20a646b5c
Maria Nikolaidou <marnicolaidou@gmail.com>
2025-01-03 15:05:13 +0100

    Fix Desktop and Mobile Navigation

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,88 +1,210 @@
-<script setup>
-import { ref } from 'vue';
+<script setup lang="ts">
+import {ref} from "vue";
 
-// TODO: Add Styling
-// TODO: Close Other Dropdowns When One is Open:
-// TODO: Close Dropdown on Outside Click:
-
-// Reactive state to track dropdown visibility
+// Track dropdown visibility
+const isMobileMenuOpen = ref(false);
 const isMoviesDropdownOpen = ref(false);
 const isSeriesDropdownOpen = ref(false);
 
+const isMobileMoviesDropdownOpen = ref(false);
+const isMobileSeriesDropdownOpen = ref(false);
+
 // Toggles the dropdown visibility
-const toggleMoviesDropdown = (event) => {
-    event.preventDefault(); // do not navigate
-    isMoviesDropdownOpen.value = !isMoviesDropdownOpen.value;
-};
-const toggleSeriesDropdown = (event) => {
-    event.preventDefault(); // do not navigate
-    isSeriesDropdownOpen.value = !isSeriesDropdownOpen.value;
-};
+function toggleMoviesDropdown(isHovered: boolean) {
+    isMoviesDropdownOpen.value = isHovered;
+}
 
+function toggleSeriesDropdown(isHovered: boolean) {
+    isSeriesDropdownOpen.value = isHovered;
+}
+
+
+function toggleMobileMoviesDropdown(event: MouseEvent) {
+    event.preventDefault();
+    isMobileMoviesDropdownOpen.value = !isMobileMoviesDropdownOpen.value;
+}
+
+function toggleMobileSeriesDropdown(event: MouseEvent) {
+  event.preventDefault();
+  isMobileSeriesDropdownOpen.value = !isMobileSeriesDropdownOpen.value;
+}
+
+function toggleMobileMenu() {
+    isMobileMenuOpen.value = !isMobileMenuOpen.value;
+    isMobileMoviesDropdownOpen.value = false;
+    isMobileSeriesDropdownOpen.value = false;
+}
 </script>
-
+<!-- Aria is for accessibility -->
 <template>
-    <header class="bg-orange-500">
-        <nav class="py-3.5 container mx-auto">
-          <ul class="flex justify-start gap-x-8">
-            <li>
-              <a href="/" aria-label="Home">
-                <img src="/vite.svg" alt="Find a Movie or Series" class="w-28 max-h-6">
-              </a>
-            </li>
-            <li><router-link to="/" class="">Home</router-link></li>
-                <li class="relative">
-                     <!-- Movies Dropdown with ARIA (accessibility) attributes -->
-                    <button  aria-haspopup="true" v-bind:aria-expanded="isMoviesDropdownOpen" v-on:click="toggleMoviesDropdown" class="">Movies</button>
-                    
-                    <!-- Submenu displays only if isDropdownOpen is true  -->
-                    <ul v-if="isMoviesDropdownOpen" class="absolute bg-orange-50 min-w-[150px] text-left text-sm rounded-md">
+    <header>
+        <!-- Navigation Bar (items always visible) - Desktop menu Desktop Menu - visible on screens bigger than 640px-->
+        <div class="sm:bg-orange-500">
+            <nav class="hidden sm:block py-3.5 container mx-auto">
+                <ul class="hidden sm:flex sm:justify-start gap-x-8">
+                    <li>
+                        <a href="/" aria-label="Home">
+                            <img
+                                src="/vite.svg"
+                                alt="Find a Movie or Series"
+                                class="w-28 max-h-6"
+                            />
+                        </a>
+                    </li>
+                    <li><router-link to="/" class="">Home</router-link></li>
+                    <li class="relative">
+                        <!-- Movies Dropdown with ARIA (accessibility) attributes -->
+                        <button
+                            aria-haspopup="true"
+                            v-bind:aria-expanded="isMoviesDropdownOpen"
+                            v-on:mouseenter="toggleMoviesDropdown(true)"
+                            v-on:mouseleave="toggleMoviesDropdown(false)"
+                        >
+                            Movies
+                        </button>
+
+                        <!-- Submenu displays only if isDropdownOpen is true  -->
+                        <ul
+                            v-if="isMoviesDropdownOpen"
+                            v-on:mouseenter="toggleMoviesDropdown(true)"
+                            v-on:mouseleave="toggleMoviesDropdown(false)"
+                            class="absolute bg-orange-300 min-w-[150px] text-left text-sm rounded-md"
+                        >
+                            <li>
+                                <router-link to="/movies/popular" class="block px-1 py-2">
+                                    Popular Movies
+                                </router-link>
+                            </li>
+                            <li>
+                                <router-link to="/movies/top-rated" class="block px-1 py-2">
+                                    Top Rated Movies
+                                </router-link>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="relative">
+                        <!-- Movies Dropdown with ARIA (accessibility) attributes -->
+                        <button
+                            aria-haspopup="true"
+                            v-bind:aria-expanded="isSeriesDropdownOpen"
+                            v-on:mouseenter="toggleSeriesDropdown(true)"
+                            v-on:mouseleave="toggleSeriesDropdown(false)"
+                            class=""
+                        >
+                            TV Series
+                        </button>
+                        <!-- Submenu displays only if isDropdownOpen is true  -->
+                        <ul
+                            v-if="isSeriesDropdownOpen"
+                            v-on:mouseenter="toggleSeriesDropdown(true)"
+                            v-on:mouseleave="toggleSeriesDropdown(false)"
+                            class="absolute bg-orange-300 min-w-[150px] text-left text-sm rounded-md"
+                        >
+                            <li>
+                                <router-link to="/tv-series/popular" class="block px-1 py-2">
+                                    Popular TV Series
+                                </router-link>
+                            </li>
+                            <li>
+                                <router-link to="/tv-series/top-rated" class="block px-1 py-2">
+                                    Top Rated TV Series
+                                </router-link>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+        <!-- Mobile Menu - visible on mobile and hidden on screens bigger than 640px -->
+        <div class="sm:hidden flex py-3.5 bg-orange-500">
+            <button
+                class="focus:outline-none"
+                aria-label="Toggle menu"
+                v-bind:aria-exapanded="isMobileMenuOpen"
+                v-on:click="toggleMobileMenu"
+            >
+                <img src="../../assets/hamburger-menu-24x24.svg" alt="menu" class="w-28 max-h-6" />
+            </button>
+            <div>
+                <a href="/" aria-label="Home" class="flex items-end">
+                    <img src="/vite.svg" alt="Find a Movie or Series" class="w-28 max-h-6 pl-10" />
+                </a>
+            </div>
+        </div>
+        <div v-if="isMobileMenuOpen" class="flex flex-col items-center bg-orange-500/75">
+            <ul class="flex flex-col gap-y-4 m-4">
+                <li><router-link to="/" class="">Home</router-link></li>
+                <li class="">
+                    <!-- Movies Dropdown with ARIA (accessibility) attributes -->
+                    <button
+                        aria-haspopup="true"
+                        v-bind:aria-expanded="isMobileMoviesDropdownOpen"
+                        v-on:click="toggleMobileMoviesDropdown"
+                    >
+                        Movies
+                    </button>
+
+                    <!-- Submenu displays only if isMobileMoviesDropdownOpen is true  -->
+                    <ul
+                        v-if="isMobileMoviesDropdownOpen"
+                        class="flex flex-col gap-y-4 mt-4 text-sm"
+                    >
                         <li>
-                          <router-link to="/movies/popular" class="block px-1 py-2">
-                            Popular Movies
-                          </router-link>
+                            <router-link to="/movies/popular">
+                                Popular Movies
+                            </router-link>
                         </li>
                         <li>
-                          <router-link to="/movies/top-rated" class="block px-1 py-2">
-                            Top Rated Movies
-                          </router-link>
+                            <router-link to="/movies/top-rated">
+                                Top Rated Movies
+                            </router-link>
                         </li>
                     </ul>
                 </li>
-                <li class="relative">
-                     <!-- Movies Dropdown with ARIA (accessibility) attributes -->
-                     <button  aria-haspopup="true" v-bind:aria-expanded="isSeriesDropdownOpen" v-on:click="toggleSeriesDropdown" class="">TV Series</button>
-                    <!-- Submenu displays only if isDropdownOpen is true  -->
-                    <ul v-if="isSeriesDropdownOpen" class="absolute bg-orange-50 min-w-[150px] text-left text-sm rounded-md">
+                <li>
+                    <!-- TV Series Dropdown with ARIA (accessibility) attributes -->
+                    <button
+                        aria-haspopup="true"
+                        v-bind:aria-expanded="isMobileSeriesDropdownOpen"
+                        v-on:click="toggleMobileSeriesDropdown"
+                      
+                    >
+                        TV Series
+                    </button>
+                    <!-- Submenu displays only if isMobileSeriesDropdownOpen is true  -->
+                    <ul
+                        v-if="isMobileSeriesDropdownOpen"
+                        class="flex flex-col gap-y-4 mt-4 text-sm"
+                    >
                         <li>
-                          <router-link to="/tv-series/popular" class="block px-1 py-2">
-                            Popular TV Series
-                          </router-link>
+                            <router-link to="/tv-series/popular">
+                                Popular TV Series
+                            </router-link>
                         </li>
                         <li>
-                          <router-link to="/tv-series/top-rated" class="block px-1 py-2">
-                            Top Rated TV Series
-                          </router-link>
+                            <router-link to="/tv-series/top-rated">
+                                Top Rated TV Series
+                            </router-link>
                         </li>
                     </ul>
                 </li>
             </ul>
-        </nav>
+        </div>
     </header>
 </template>
 
 <!-- can give the navigation and submenus a background colour, set the font size and colour, and add a hover effect to the links. We can also use CSS to hide the submenus by default, and display them only when the parent link hovers over -->
 <style scoped>
-
 /* Remember the class router-link-active or .router-link-exact-active are dded when we are in the current route. We can style the active link! */
 a.router-link-active {
-  color: #fffbeb
+    color: #fffbeb;
 }
 
 /* https://tailwindcss.com/docs/display#block-and-inline */
-li {
-  display: inline-block;
-}
+/* li {
+    display: inline-block;
+} */
 
 /* a {
   color: #000;
--- src/components/movies/ListPopularMovies.vue
+++ src/components/movies/ListPopularMovies.vue
@@ -4,63 +4,6 @@
 <!-- https://vuejs.org/guide/essentials/list#v-for -->
 <!-- For now this lists the most popular movies -->
 <!-- TODO: this should be updated to just list any movies according to selection -->
-<template>
-    <!-- TODO: Display a loading message or spinner until data is available -->
-    <div v-if="!popularMovies">
-        <p>Loading movies...</p>
-    </div>
-
-    <div v-else>
-        <h3 class="text-lg font-medium mb-4">Here's a list of the most popular movies in {{ countryName }}</h3>
-
-        <!-- Render only if popularMovies has data -->
-         <ul>
-             <li
-                 v-for="popularMovie in popularMovies.results"
-                 v-bind:key="popularMovie.id"
-                 class="flex justify-between gap-x-6 py-5"
-             >
-                 <div class="flex min-w-0 gap-x-4">
-                     <!-- check here how to list it https://developer.themoviedb.org/docs/image-basics -->
-                     <img
-                         class="size-fit flex-none rounded-lg bg-gray-50"
-                         :src="`https://image.tmdb.org/t/p/w200${popularMovie.poster_path}`"
-                         alt=""
-                     />
-                     <div class="min-w-0 flex-auto">
-                         <!-- /6 is the line height -->
-                         <p class="text-lg/6 font-semibold text-gray-900">{{ popularMovie.title }}</p>
-                         <p class="mt-1 truncate text-xs/5 text-gray-500">
-                             {{ genre(popularMovie.genre_ids) }}
-                         </p>
-                         <p class="mt-3 truncate text-base/5 text-gray-700">Overview </p>
-                         <p class="mt-5 text-sm/6 font-semibold text-gray-600">{{ popularMovie.overview }}</p>
-                     </div>
-                     
-                 </div>
-                 
-                 
-                 <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                     <p class="text-sm/6 text-gray-900">Popularity: {{ popularMovie.popularity }}</p>
-                     <p class="mt-1 text-xs/5 text-gray-500">
-                         Average Vote: {{ popularMovie.vote_average }}
-                     </p>
-                     <p class="mt-1 text-xs/5 text-gray-500">
-                         Release Date: {{ popularMovie.release_date }}
-                     </p>
-                 </div>
-             </li>
-
-         </ul>
-
-        <Pagination
-            :totalPages="popularMovies.total_pages"
-            :perPage="popularMovies.results.length"
-            :currentPage="popularMovies.page"
-            v-on:pagechanged="onPageChange"
-        ></Pagination>
-    </div>
-</template>
 
 <script lang="ts" setup>
 import Pagination from "../Pagination.vue";
@@ -106,3 +49,63 @@ async function onPageChange(page: number) {
     popularMovies.value = await getPopularMoviesByRegion("GB", page);
 }
 </script>
+
+<template>
+    <div class="container mx-auto max-w-5xl px-4 py-8">
+        <!-- TODO: Display a loading message or spinner until data is available -->
+        <div v-if="!popularMovies">
+            <p>Loading movies...</p>
+        </div>
+    
+        <div v-else>
+            <h3 class="text-lg font-medium mb-4">Here's a list of the most popular movies in {{ countryName }}</h3>
+    
+            <!-- Render only if popularMovies has data -->
+             <ul>
+                 <li
+                     v-for="popularMovie in popularMovies.results"
+                     v-bind:key="popularMovie.id"
+                     class="flex justify-between gap-x-6 py-5"
+                 >
+                     <div class="flex min-w-0 gap-x-4">
+                         <!-- check here how to list it https://developer.themoviedb.org/docs/image-basics -->
+                         <img
+                             class="size-fit flex-none rounded-lg bg-gray-50"
+                             :src="`https://image.tmdb.org/t/p/w200${popularMovie.poster_path}`"
+                             alt=""
+                         />
+                         <div class="min-w-0 flex-auto">
+                             <!-- /6 is the line height -->
+                             <p class="text-lg/6 font-semibold text-gray-900">{{ popularMovie.title }}</p>
+                             <p class="mt-1 text-xs/5 text-gray-500">
+                                 {{ genre(popularMovie.genre_ids) }}
+                             </p>
+                             <p class="mt-3 text-base/5 text-gray-700">Overview </p>
+                             <p class="mt-5 text-sm/6 font-semibold text-gray-600">{{ popularMovie.overview }}</p>
+                         </div>
+                         
+                     </div>
+                     
+                     
+                     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                         <p class="text-sm/6 text-gray-900">Popularity: {{ popularMovie.popularity }}</p>
+                         <p class="mt-1 text-xs/5 text-gray-500">
+                             Average Vote: {{ popularMovie.vote_average }}
+                         </p>
+                         <p class="mt-1 text-xs/5 text-gray-500">
+                             Release Date: {{ popularMovie.release_date }}
+                         </p>
+                     </div>
+                 </li>
+    
+             </ul>
+    
+            <Pagination
+                :totalPages="popularMovies.total_pages"
+                :perPage="popularMovies.results.length"
+                :currentPage="popularMovies.page"
+                v-on:pagechanged="onPageChange"
+            ></Pagination>
+        </div>
+    </div>
+</template>
